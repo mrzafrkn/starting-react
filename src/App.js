8a9f7f47e83c646f9c2f11af33965cf912a425fd
Mirza Furkan Kalkan <furkankalkan502@gmail.com>
2024-09-25 10:56:49 +0300

    First update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React dünyasına hoş geldin bebeğim!</h1>
+        <h3>Benim bebeğimin gazı varmış Metpamid içsin</h3>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
